052922918d10159172d4203ca94dd5bde83255d8
BaNru <admin@g63.ru>
2021-07-07 10:25:44 +0400

    Вернул старый адрес получения треков. Ничего не понимаю, то одно работает, то другое

--- js/background.js
+++ js/background.js
@@ -11,7 +11,8 @@ function getTrackXHRBG(attr) {
 			return resolve(inLS);
 		}
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'https://ilogisticsaddress.aliexpress.ru/ajax_logistics_track.htm?orderId=' + ordernumber);
+		xhr.open('GET', 'https://track.aliexpress.com/logisticsdetail.htm?tradeId=' + ordernumber);
+		//xhr.open('GET', 'https://ilogisticsaddress.aliexpress.ru/ajax_logistics_track.htm?orderId=' + ordernumber);
 		xhr.onload = ()=>{
 			var tracknumber = [];
 			if (/"logisticsNo":"(.*?)"/.exec(xhr.responseText)) {
